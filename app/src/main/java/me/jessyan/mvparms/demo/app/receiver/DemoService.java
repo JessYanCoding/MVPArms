2a33793944c4e6a10fe24f8fae39e69e9da294c3
jessyan <jessyan@foxmail.com>
2016-09-07 17:00:33 +0800

    add service package

---
+++ app/src/main/java/me/jessyan/mvparms/demo/app/receiver/DemoService.java
@@ -0,0 +1,14 @@
+package me.jessyan.mvparms.demo.app.receiver;
+
+import com.jess.arms.base.BaseService;
+
+/**
+ * Created by jess on 9/7/16 16:59
+ * Contact with jess.yan.effort@gmail.com
+ */
+public class DemoService extends BaseService {
+    @Override
+    public void init() {
+
+    }
+}
