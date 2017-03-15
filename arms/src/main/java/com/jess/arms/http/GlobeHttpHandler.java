ae2cdad279674cd9248d58cfe1a9fcac626e860f
jessyan <jessyan@foxmail.com>
2017-03-15 11:59:03 +0800

    improve GlobeHttpHandler.EMPTY

--- arms/src/main/java/com/jess/arms/http/GlobeHttpHandler.java
+++ arms/src/main/java/com/jess/arms/http/GlobeHttpHandler.java
@@ -16,11 +16,13 @@ public interface GlobeHttpHandler {
     GlobeHttpHandler EMPTY = new GlobeHttpHandler() {
         @Override
         public Response onHttpResultResponse(String httpResult, Interceptor.Chain chain, Response response) {
+            //不管是否处理,都必须将response返回出去
             return response;
         }
 
         @Override
         public Request onHttpRequestBefore(Interceptor.Chain chain, Request request) {
+            //不管是否处理,都必须将request返回出去
             return request;
         }
     };
