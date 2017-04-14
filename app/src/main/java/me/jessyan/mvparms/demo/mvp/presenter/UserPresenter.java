e4d3d28327f48c62227c27b3ab92a7c2dbf177a5
xiaobailong24 <xiaobailong24@gmail.com>
2017-04-14 12:50:32 +0800

    x)Update demo using rx2

--- app/src/main/java/me/jessyan/mvparms/demo/mvp/presenter/UserPresenter.java
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/presenter/UserPresenter.java
@@ -2,9 +2,10 @@ package me.jessyan.mvparms.demo.mvp.presenter;
 
 import android.app.Application;
 
-import com.jess.arms.integration.AppManager;
 import com.jess.arms.base.DefaultAdapter;
 import com.jess.arms.di.scope.ActivityScope;
+import com.jess.arms.http.RetryWithDelay;
+import com.jess.arms.integration.AppManager;
 import com.jess.arms.mvp.BasePresenter;
 import com.jess.arms.utils.PermissionUtil;
 import com.jess.arms.utils.RxUtils;
@@ -14,14 +15,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import me.jessyan.mvparms.demo.mvp.contract.UserContract;
 import me.jessyan.mvparms.demo.mvp.model.entity.User;
 import me.jessyan.mvparms.demo.mvp.ui.adapter.UserAdapter;
-import me.jessyan.rxerrorhandler.core.RxErrorHandler;
-import me.jessyan.rxerrorhandler.handler.ErrorHandleSubscriber;
-import me.jessyan.rxerrorhandler.handler.RetryWithDelay;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by jess on 9/4/16 10:59
@@ -29,7 +27,6 @@ import rx.schedulers.Schedulers;
  */
 @ActivityScope
 public class UserPresenter extends BasePresenter<UserContract.Model, UserContract.View> {
-    private RxErrorHandler mErrorHandler;
     private AppManager mAppManager;
     private Application mApplication;
     private List<User> mUsers = new ArrayList<>();
@@ -40,11 +37,10 @@ public class UserPresenter extends BasePresenter<UserContract.Model, UserContrac
 
 
     @Inject
-    public UserPresenter(UserContract.Model model, UserContract.View rootView, RxErrorHandler handler
-            , AppManager appManager, Application application) {
+    public UserPresenter(UserContract.Model model, UserContract.View rootView,
+                         AppManager appManager, Application application) {
         super(model, rootView);
         this.mApplication = application;
-        this.mErrorHandler = handler;
         this.mAppManager = appManager;
         mAdapter = new UserAdapter(mUsers);
         mRootView.setAdapter(mAdapter);//设置Adapter
@@ -54,10 +50,11 @@ public class UserPresenter extends BasePresenter<UserContract.Model, UserContrac
         //请求外部存储权限用于适配android6.0的权限管理机制
         PermissionUtil.externalStorage(() -> {
             //request permission success, do something.
-        }, mRootView.getRxPermissions(), mRootView, mErrorHandler);
+        }, mRootView.getRxPermissions(), mRootView);
 
 
-        if (pullToRefresh) lastUserId = 1;//上拉刷新默认只请求第一页
+        if (pullToRefresh)
+            lastUserId = 1;//上拉刷新默认只请求第一页
 
         //关于RxCache缓存库的使用请参考 http://www.jianshu.com/p/b58ef6b0624b
 
@@ -71,12 +68,14 @@ public class UserPresenter extends BasePresenter<UserContract.Model, UserContrac
         mModel.getUsers(lastUserId, isEvictCache)
                 .subscribeOn(Schedulers.io())
                 .retryWhen(new RetryWithDelay(3, 2))//遇到错误时重试,第一个参数为重试几次,第二个参数为重试的间隔
-                .doOnSubscribe(() -> {
+                .doOnSubscribe(disposable -> {
+                    addDispose(disposable);
                     if (pullToRefresh)
-                        mRootView.showLoading();//显示上拉刷新的进度条
+                        mRootView.hideLoading();//隐藏上拉刷新的进度条
                     else
-                        mRootView.startLoadMore();//显示下拉加载更多的进度条
-                }).subscribeOn(AndroidSchedulers.mainThread())
+                        mRootView.endLoadMore();//隐藏下拉加载更多的进度条
+                })
+                .subscribeOn(AndroidSchedulers.mainThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doAfterTerminate(() -> {
                     if (pullToRefresh)
@@ -84,19 +83,17 @@ public class UserPresenter extends BasePresenter<UserContract.Model, UserContrac
                     else
                         mRootView.endLoadMore();//隐藏下拉加载更多的进度条
                 })
-                .compose(RxUtils.<List<User>>bindToLifecycle(mRootView))//使用RXlifecycle,使subscription和activity一起销毁
-                .subscribe(new ErrorHandleSubscriber<List<User>>(mErrorHandler) {
-                    @Override
-                    public void onNext(List<User> users) {
-                        lastUserId = users.get(users.size() - 1).getId();//记录最后一个id,用于下一次请求
-                        if (pullToRefresh) mUsers.clear();//如果是上拉刷新则清空列表
-                        preEndIndex = mUsers.size();//更新之前列表总长度,用于确定加载更多的起始位置
-                        mUsers.addAll(users);
-                        if (pullToRefresh)
-                            mAdapter.notifyDataSetChanged();
-                        else
-                            mAdapter.notifyItemRangeInserted(preEndIndex, users.size());
-                    }
+                .compose(RxUtils.bindToLifecycle(mRootView))//使用RXlifecycle,使subscription和activity一起销毁
+                .subscribe(users -> {
+                    lastUserId = users.get(users.size() - 1).getId();//记录最后一个id,用于下一次请求
+                    if (pullToRefresh)
+                        mUsers.clear();//如果是上拉刷新则清空列表
+                    preEndIndex = mUsers.size();//更新之前列表总长度,用于确定加载更多的起始位置
+                    mUsers.addAll(users);
+                    if (pullToRefresh)
+                        mAdapter.notifyDataSetChanged();
+                    else
+                        mAdapter.notifyItemRangeInserted(preEndIndex, users.size());
                 });
     }
 
@@ -105,7 +102,6 @@ public class UserPresenter extends BasePresenter<UserContract.Model, UserContrac
         super.onDestroy();
         this.mAdapter = null;
         this.mUsers = null;
-        this.mErrorHandler = null;
         this.mAppManager = null;
         this.mApplication = null;
     }
