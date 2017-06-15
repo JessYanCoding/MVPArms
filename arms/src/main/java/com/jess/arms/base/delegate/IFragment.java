3a1be19a2f70ca59bb15eff77fbe55ecedb364cd
jessyan <jessyan@foxmail.com>
2017-06-15 10:56:40 +0800

    Update IFragment comment

--- arms/src/main/java/com/jess/arms/base/delegate/IFragment.java
+++ arms/src/main/java/com/jess/arms/base/delegate/IFragment.java
@@ -30,9 +30,9 @@ public interface IFragment {
      * 建议在有多个需要让外界调用的方法时,统一传Message,通过what字段,来区分不同的方法,在setData
      * 方法中就可以switch做不同的操作,这样就可以用统一的入口方法做不同的事
      *
-     * 使用此方法时请注意调用时fragment的生命周期,如果调用此setData方法时onActivityCreated
-     * 还没执行,setData里调用presenter的方法时,是会报空的,因为dagger注入是在onActivityCreated
-     * 方法中执行的,如果要做一些初始化操作,可以不必让外部调setData,在initData中初始化就可以了
+     * 使用此方法时请注意调用时fragment的生命周期,如果调用此setData方法时onCreate还没执行
+     * setData里却调用了presenter的方法时,是会报空的,因为dagger注入是在onCreated方法中执行的,然后才创建的presenter
+     * 如果要做一些初始化操作,可以不必让外部调setData,在initData中初始化就可以了
      *
      * @param data
      */
