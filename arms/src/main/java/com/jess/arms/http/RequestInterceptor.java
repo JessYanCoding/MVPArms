e1b7487dcbf8cf6439e88ff5d2ecbfcd3443a42e
jessyan <jessyan@foxmail.com>
2017-09-19 18:15:26 +0800

    Improve comment

--- arms/src/main/java/com/jess/arms/http/RequestInterceptor.java
+++ arms/src/main/java/com/jess/arms/http/RequestInterceptor.java
@@ -1,18 +1,18 @@
 /**
-  * Copyright 2017 JessYan
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2017 JessYan
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jess.arms.http;
 
 import android.support.annotation.Nullable;
@@ -191,6 +191,13 @@ public class RequestInterceptor implements Interceptor {
         }
     }
 
+    /**
+     * 解析请求服务器的请求参数
+     *
+     * @param body
+     * @return
+     * @throws UnsupportedEncodingException
+     */
     public static String parseParams(RequestBody body) throws UnsupportedEncodingException {
         if (isParseable(body.contentType())) {
             try {
@@ -210,6 +217,12 @@ public class RequestInterceptor implements Interceptor {
         return "This params isn't parsed";
     }
 
+    /**
+     * 是否可以解析
+     *
+     * @param mediaType
+     * @return
+     */
     public static boolean isParseable(MediaType mediaType) {
         if (mediaType == null) return false;
         return mediaType.toString().toLowerCase().contains("text")
