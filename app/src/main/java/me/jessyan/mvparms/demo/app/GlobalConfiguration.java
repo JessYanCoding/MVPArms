aca83931461df35fc232a9c8c4dd1e2a7ceb07a1
jessyan <jessyan@foxmail.com>
2017-11-09 19:31:02 +0800

    Improve comment

--- app/src/main/java/me/jessyan/mvparms/demo/app/GlobalConfiguration.java
+++ app/src/main/java/me/jessyan/mvparms/demo/app/GlobalConfiguration.java
@@ -135,7 +135,7 @@ public final class GlobalConfiguration implements ConfigModule {
 
             @Override
             public void onFragmentCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
-                // 在配置变化的时候将这个 Fragment 保存下来,在 Activity 由于配置变化重建是重复利用已经创建的Fragment。
+                // 在配置变化的时候将这个 Fragment 保存下来,在 Activity 由于配置变化重建时重复利用已经创建的 Fragment。
                 // https://developer.android.com/reference/android/app/Fragment.html?hl=zh-cn#setRetainInstance(boolean)
                 // 如果在 XML 中使用 <Fragment/> 标签,的方式创建 Fragment 请务必在标签中加上 android:id 或者 android:tag 属性,否则 setRetainInstance(true) 无效
                 // 在 Activity 中绑定少量的 Fragment 建议这样做,如果需要绑定较多的 Fragment 不建议设置此参数,如 ViewPager 需要展示较多 Fragment
