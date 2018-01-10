b9d26dcc107891393bce23d11a3bb10d378eff5d
passin <zengbinbinpassin@foxmail.com>
2018-01-10 12:54:29 +0800

    update deviceUtils

--- arms/src/main/java/com/jess/arms/utils/DeviceUtils.java
+++ arms/src/main/java/com/jess/arms/utils/DeviceUtils.java
@@ -282,7 +282,7 @@ public class DeviceUtils {
      */
     public static boolean hasInternet(Context context) {
         boolean flag;
-        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager manager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         if (manager != null && manager.getActiveNetworkInfo() != null)
             flag = true;
         else
@@ -636,7 +636,7 @@ public class DeviceUtils {
      */
     public static boolean isWifiOpen(Context context) {
         boolean isWifiConnect = false;
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         // check the networkInfos numbers
         NetworkInfo[] networkInfos = cm.getAllNetworkInfo();
         for (int i = 0; i < networkInfos.length; i++) {
@@ -752,7 +752,7 @@ public class DeviceUtils {
      */
     public static int getNetworkType(Context context) {
         int netType = 0;
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if (networkInfo == null) {
             return netType;
@@ -774,7 +774,7 @@ public class DeviceUtils {
     }
 
     public static boolean netIsConnected(Context context) {
-        ConnectivityManager connectMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectMgr = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         //手机网络连接状态
         NetworkInfo mobNetInfo = connectMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
         //WIFI连接状态
