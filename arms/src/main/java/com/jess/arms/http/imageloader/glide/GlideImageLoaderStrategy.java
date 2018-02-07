a18e6fed99486d05a356a2eda202d93dc4b24f9f
jessyan <jessyan@foxmail.com>
2018-02-07 15:49:01 +0800

    GlideImageLoaderStrategy

--- arms/src/main/java/com/jess/arms/http/imageloader/glide/GlideImageLoaderStrategy.java
+++ arms/src/main/java/com/jess/arms/http/imageloader/glide/GlideImageLoaderStrategy.java
@@ -1,18 +1,18 @@
 /**
-  * Copyright 2017 JessYan
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2017 JessYan
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jess.arms.http.imageloader.glide;
 
 import android.content.Context;
@@ -79,6 +79,9 @@ public class GlideImageLoaderStrategy implements BaseImageLoaderStrategy<ImageCo
                 break;
             case 4:
                 glideRequest.diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
+                break;
+            default:
+                glideRequest.diskCacheStrategy(DiskCacheStrategy.ALL);
                 break;
         }
         if (config.getTransformation() != null) {//glide用它来改变图形的形状
