9dfaee5835a0656c2ae588e0b565c842d9292c0e
JessYan <jessyan@foxmail.com>
2018-04-26 14:03:08 +0800

    Rename Basejson to BaseResponse

--- app/src/main/java/me/jessyan/mvparms/demo/mvp/model/entity/BaseResponse.java
+++ app/src/main/java/me/jessyan/mvparms/demo/mvp/model/entity/BaseResponse.java
@@ -22,14 +22,14 @@ import me.jessyan.mvparms.demo.mvp.model.api.Api;
 /**
  * ================================================
  * 如果你服务器返回的数据格式固定为这种方式(这里只提供思想,服务器返回的数据格式可能不一致,可根据自家服务器返回的格式作更改)
- * 替换范型即可重用 {@link BaseJson}
+ * 指定范型即可改变 {@code data} 字段的类型, 达到重用 {@link BaseResponse}, 如果你实在看不懂, 请忽略
  * <p>
  * Created by JessYan on 26/09/2016 15:19
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
-public class BaseJson<T> implements Serializable {
+public class BaseResponse<T> implements Serializable {
     private T data;
     private String code;
     private String msg;
