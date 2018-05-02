fb09c548b015d96b80b5dfa0e4a496e871f69aa4
JessYan <jessyan@foxmail.com>
2018-05-02 16:29:34 +0800

    Improve IView's comment

--- arms/src/main/java/com/jess/arms/mvp/IView.java
+++ arms/src/main/java/com/jess/arms/mvp/IView.java
@@ -28,7 +28,7 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * 框架要求框架中的每个 View 都需要实现此类, 以满足规范
  * <p>
  * 为了满足部分人的诉求以及向下兼容, {@link IView} 中的部分方法使用 JAVA 1.8 的默认方法实现, 这样实现类可以按实际需求选择是否实现某些方法
- * 不实现则使用默认方法的中逻辑, 不清楚默认方法的请自行学习
+ * 不实现则使用默认方法中的逻辑, 不清楚默认方法的请自行学习
  *
  * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#2.4.2">View wiki 官方文档</a>
  * Created by JessYan on 4/22/2016
