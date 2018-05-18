42531f315649280945e5b439cf6b2109f1efd8e9
JessYan <jessyan@foxmail.com>
2018-05-18 11:34:22 +0800

    Improve GlobalHttpHandlerImpl

--- app/src/main/java/me/jessyan/mvparms/demo/app/GlobalHttpHandlerImpl.java
+++ app/src/main/java/me/jessyan/mvparms/demo/app/GlobalHttpHandlerImpl.java
@@ -47,14 +47,21 @@ public class GlobalHttpHandlerImpl implements GlobalHttpHandler {
         this.context = context;
     }
 
+    /**
+     * 这里可以先客户端一步拿到每一次 Http 请求的结果, 可以先解析成 Json, 再做一些操作, 如检测到 token 过期后
+     * 重新请求 token, 并重新执行请求
+     *
+     * @param httpResult 服务器返回的结果 (已被框架自动转换为字符串)
+     * @param chain {@link okhttp3.Interceptor.Chain}
+     * @param response {@link Response}
+     * @return
+     */
     @Override
     public Response onHttpResultResponse(String httpResult, Interceptor.Chain chain, Response response) {
-                    /* 这里可以先客户端一步拿到每一次http请求的结果,可以解析成json,做一些操作,如检测到token过期后
-                       重新请求token,并重新执行请求 */
-
         if (!TextUtils.isEmpty(httpResult) && RequestInterceptor.isJson(response.body().contentType())) {
             try {
-                List<User> list = ArmsUtils.obtainAppComponentFromContext(context).gson().fromJson(httpResult, new TypeToken<List<User>>() {}.getType());
+                List<User> list = ArmsUtils.obtainAppComponentFromContext(context).gson().fromJson(httpResult, new TypeToken<List<User>>() {
+                }.getType());
                 User user = list.get(0);
                 Timber.w("Result ------> " + user.getLogin() + "    ||   Avatar_url------> " + user.getAvatarUrl());
             } catch (Exception e) {
@@ -63,26 +70,31 @@ public class GlobalHttpHandlerImpl implements GlobalHttpHandler {
             }
         }
 
-                 /* 这里如果发现token过期,可以先请求最新的token,然后在拿新的token放入request里去重新请求
-                    注意在这个回调之前已经调用过proceed,所以这里必须自己去建立网络请求,如使用okhttp使用新的request去请求
-                    create a new request and modify it accordingly using the new token
-                    Request newRequest = chain.request().newBuilder().header("token", newToken)
-                                         .build();
+        /* 这里如果发现 token 过期, 可以先请求最新的 token, 然后在拿新的 token 放入 Request 里去重新请求
+        注意在这个回调之前已经调用过 proceed(), 所以这里必须自己去建立网络请求, 如使用 Okhttp 使用新的 Request 去请求
+        create a new request and modify it accordingly using the new token
+        Request newRequest = chain.request().newBuilder().header("token", newToken)
+                             .build();
 
-                    retry the request
+        retry the request
 
-                    response.body().close();
-                    如果使用okhttp将新的请求,请求成功后,将返回的response  return出去即可
-                    如果不需要返回新的结果,则直接把response参数返回出去 */
-
+        response.body().close();
+        如果使用 Okhttp 将新的请求, 请求成功后, 再将 Okhttp 返回的 Response return 出去即可
+        如果不需要返回新的结果, 则直接把参数 response 返回出去即可*/
         return response;
     }
 
-    // 这里可以在请求服务器之前可以拿到request,做一些操作比如给request统一添加token或者header以及参数加密等操作
+    /**
+     * 这里可以在请求服务器之前拿到 {@link Request}, 做一些操作比如给 {@link Request} 统一添加 token 或者 header 以及参数加密等操作
+     *
+     * @param chain {@link okhttp3.Interceptor.Chain}
+     * @param request {@link Request}
+     * @return
+     */
     @Override
     public Request onHttpRequestBefore(Interceptor.Chain chain, Request request) {
-                    /* 如果需要再请求服务器之前做一些操作,则重新返回一个做过操作的的request如增加header,不做操作则直接返回request参数
-                       return chain.request().newBuilder().header("token", tokenId)
+        /* 如果需要再请求服务器之前做一些操作, 则重新返回一个做过操作的的 Request 如增加 Header, 不做操作则直接返回参数 request
+        return chain.request().newBuilder().header("token", tokenId)
                               .build(); */
         return request;
     }
