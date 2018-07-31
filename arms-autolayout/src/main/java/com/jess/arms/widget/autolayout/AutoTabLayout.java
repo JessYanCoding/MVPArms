7e7c3d080dd5e88a15bf5d84bcb4e236d4f1a3f1
JessYan <jessyan@foxmail.com>
2018-07-31 15:15:32 +0800

    Improve AutoTabLayout and AutoToolbar

--- arms-autolayout/src/main/java/com/jess/arms/widget/autolayout/AutoTabLayout.java
+++ arms-autolayout/src/main/java/com/jess/arms/widget/autolayout/AutoTabLayout.java
@@ -24,7 +24,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.jess.arms.widget.R;
 import com.zhy.autolayout.utils.AutoUtils;
 import com.zhy.autolayout.utils.DimenUtils;
 
--- arms-autolayout/src/main/java/com/jess/arms/widget/autolayout/AutoToolbar.java
+++ arms-autolayout/src/main/java/com/jess/arms/widget/autolayout/AutoToolbar.java
@@ -24,7 +24,6 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
-import com.jess.arms.widget.R;
 import com.zhy.autolayout.AutoLayoutInfo;
 import com.zhy.autolayout.utils.AutoLayoutHelper;
 import com.zhy.autolayout.utils.AutoUtils;
