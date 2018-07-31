bf70bf5f2509ee9a3eee2e09a41a02569d38cfd4
JessYan <jessyan@foxmail.com>
2018-07-31 15:53:42 +0800

    Improve AppManager

--- arms/src/main/java/com/jess/arms/integration/AppManager.java
+++ arms/src/main/java/com/jess/arms/integration/AppManager.java
@@ -22,6 +22,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 
@@ -234,8 +235,9 @@ public final class AppManager {
      *
      * @return
      */
+    @Nullable
     public Activity getCurrentActivity() {
-        return mCurrentActivity != null ? mCurrentActivity : null;
+        return mCurrentActivity;
     }
 
     /**
@@ -248,6 +250,7 @@ public final class AppManager {
      *
      * @return
      */
+    @Nullable
     public Activity getTopActivity() {
         if (mActivityList == null) {
             Timber.tag(TAG).w("mActivityList == null when getTopActivity()");
