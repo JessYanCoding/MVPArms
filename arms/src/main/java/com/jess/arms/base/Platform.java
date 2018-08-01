e22a323e619645f8676be01d072201863a03459e
JessYan <jessyan@foxmail.com>
2018-08-01 18:33:41 +0800

    Add DEPENDENCY_ANDROID_EVENTBUS and DEPENDENCY_EVENTBUS on Platform

--- arms/src/main/java/com/jess/arms/base/Platform.java
+++ arms/src/main/java/com/jess/arms/base/Platform.java
@@ -26,11 +26,15 @@ public class Platform {
     public static final boolean DEPENDENCY_AUTO_LAYOUT;
     public static final boolean DEPENDENCY_SUPPORT_DESIGN;
     public static final boolean DEPENDENCY_GLIDE;
+    public static final boolean DEPENDENCY_ANDROID_EVENTBUS;
+    public static final boolean DEPENDENCY_EVENTBUS;
 
     static {
         DEPENDENCY_AUTO_LAYOUT = findClassByClassName("com.zhy.autolayout.AutoLayoutInfo");
         DEPENDENCY_SUPPORT_DESIGN = findClassByClassName("android.support.design.widget.Snackbar");
         DEPENDENCY_GLIDE = findClassByClassName("com.bumptech.glide.Glide");
+        DEPENDENCY_ANDROID_EVENTBUS = findClassByClassName("org.simple.eventbus.EventBus");
+        DEPENDENCY_EVENTBUS = findClassByClassName("org.greenrobot.eventbus.EventBus");
     }
 
     private static boolean findClassByClassName(String className) {
---
+++ arms/src/main/java/com/jess/arms/integration/EventBusManager.java
@@ -0,0 +1,11 @@
+package com.jess.arms.integration;
+
+/**
+ * ================================================
+ * Created by JessYan on 2018/8/1 15:28
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public final class EventBusManager {
+}
