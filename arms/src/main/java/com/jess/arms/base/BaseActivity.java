17c7003323527606d5aec2c3668b55aadc134d9e
JessYan <jessyan@foxmail.com>
2018-08-02 17:42:18 +0800

    Improve useEventBus's comments

--- arms/src/main/java/com/jess/arms/base/BaseActivity.java
+++ arms/src/main/java/com/jess/arms/base/BaseActivity.java
@@ -116,9 +116,13 @@ public abstract class BaseActivity<P extends IPresenter> extends AppCompatActivi
     }
 
     /**
-     * 是否使用eventBus,默认为使用(true)，
+     * 是否使用 EventBus
+     * Arms 核心库现在并不会依赖某个 EventBus, 要想使用 EventBus, 还请在项目中自行依赖对应的 EventBus
+     * 现在支持两种 EventBus, greenrobot 的 EventBus 和畅销书 《Android源码设计模式解析与实战》的作者 何红辉 所作的 AndroidEventBus
+     * 确保依赖后, 将此方法返回 true, Arms 会自动检测您依赖的 EventBus, 并自动注册
+     * 这种做法可以让使用者有自行选择三方库的权利, 并且还可以减轻 Arms 的体积
      *
-     * @return
+     * @return 返回 {@code true} (默认为使用 {@code true}), Arms 会自动注册 EventBus
      */
     @Override
     public boolean useEventBus() {
--- arms/src/main/java/com/jess/arms/base/delegate/IActivity.java
+++ arms/src/main/java/com/jess/arms/base/delegate/IActivity.java
@@ -29,8 +29,6 @@ import com.jess.arms.integration.ActivityLifecycle;
 import com.jess.arms.integration.cache.Cache;
 import com.jess.arms.integration.cache.LruCache;
 
-import org.simple.eventbus.EventBus;
-
 /**
  * ================================================
  * 框架要求框架中的每个 {@link Activity} 都需要实现此类,以满足规范
@@ -61,9 +59,13 @@ public interface IActivity {
     void setupActivityComponent(@NonNull AppComponent appComponent);
 
     /**
-     * 是否使用 {@link EventBus}
+     * 是否使用 EventBus
+     * Arms 核心库现在并不会依赖某个 EventBus, 要想使用 EventBus, 还请在项目中自行依赖对应的 EventBus
+     * 现在支持两种 EventBus, greenrobot 的 EventBus 和畅销书 《Android源码设计模式解析与实战》的作者 何红辉 所作的 AndroidEventBus
+     * 确保依赖后, 将此方法返回 true, Arms 会自动检测您依赖的 EventBus, 并自动注册
+     * 这种做法可以让使用者有自行选择三方库的权利, 并且还可以减轻 Arms 的体积
      *
-     * @return
+     * @return 返回 {@code true}, Arms 会自动注册 EventBus
      */
     boolean useEventBus();
 
