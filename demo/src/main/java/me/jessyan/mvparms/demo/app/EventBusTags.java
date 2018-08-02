1848f1e6ea488cd03fc2619e1028f9df6eb5fa34
JessYan <jessyan@foxmail.com>
2018-08-02 18:18:08 +0800

    Improve EventBusTags's comments

--- demo/src/main/java/me/jessyan/mvparms/demo/app/EventBusTags.java
+++ demo/src/main/java/me/jessyan/mvparms/demo/app/EventBusTags.java
@@ -15,11 +15,11 @@
  */
 package me.jessyan.mvparms.demo.app;
 
-import org.simple.eventbus.EventBus;
-
 /**
  * ================================================
- * 放置 {@link EventBus} 的 Tag ,便于检索
+ * 放置 AndroidEventBus 的 Tag, 便于检索
+ * Arms 核心库现在并不会依赖某个 EventBus, 要想使用 EventBus, 还请在项目中自行依赖对应的 EventBus
+ * 现在支持两种 EventBus, greenrobot 的 EventBus 和畅销书 《Android源码设计模式解析与实战》的作者 何红辉 所作的 AndroidEventBus
  *
  * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#3.5">EventBusTags wiki 官方文档</a>
  * Created by JessYan on 8/30/2016 16:39
