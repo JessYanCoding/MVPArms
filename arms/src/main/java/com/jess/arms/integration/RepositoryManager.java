56a73daa2981ebcd0ac6f0af1344453ac732ce5a
JessYan <jessyan@foxmail.com>
2018-08-30 14:22:33 +0800

    Improve RepositoryManager

--- arms/src/main/java/com/jess/arms/integration/RepositoryManager.java
+++ arms/src/main/java/com/jess/arms/integration/RepositoryManager.java
@@ -18,19 +18,23 @@ package com.jess.arms.integration;
 import android.app.Application;
 import android.content.Context;
 import android.support.annotation.Nullable;
+
 import com.jess.arms.integration.cache.Cache;
 import com.jess.arms.integration.cache.CacheType;
 import com.jess.arms.mvp.IModel;
 import com.jess.arms.utils.Preconditions;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import dagger.Lazy;
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
 import io.rx_cache2.internal.RxCache;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import retrofit2.Retrofit;
 
 /**
@@ -64,13 +68,23 @@ public class RepositoryManager implements IRepositoryManager {
 
     /**
      * 根据传入的 Class 获取对应的 Retrofit service
+     *
+     * @param serviceClass ApiService class
+     * @param <T>          ApiService class
+     * @return ApiService
      */
     @Override
-    public synchronized <T> T obtainRetrofitService(Class<T> service) {
-        return createWrapperService(service);
+    public synchronized <T> T obtainRetrofitService(Class<T> serviceClass) {
+        return createWrapperService(serviceClass);
     }
 
-
+    /**
+     * 根据 https://zhuanlan.zhihu.com/p/40097338 对 Retrofit 进行的优化
+     *
+     * @param serviceClass ApiService class
+     * @param <T>          ApiService class
+     * @return ApiService
+     */
     private <T> T createWrapperService(Class<T> serviceClass) {
         // 通过二次代理，对 Retrofit 代理方法的调用包进新的 Observable 里在 io 线程执行。
         return (T) Proxy.newProxyInstance(serviceClass.getClassLoader(),
@@ -84,7 +98,8 @@ public class RepositoryManager implements IRepositoryManager {
                                 final T service = getRetrofitService(serviceClass);
                                 // 执行真正的 Retrofit 动态代理的方法
                                 return ((Observable) getRetrofitMethod(service, method)
-                                        .invoke(service, args));
+                                        .invoke(service, args))
+                                        .subscribeOn(Schedulers.io());
                             }).subscribeOn(Schedulers.single());
                         }
                         // 返回值不是 Observable 的话不处理
@@ -94,16 +109,23 @@ public class RepositoryManager implements IRepositoryManager {
                 });
     }
 
-    private <T> T getRetrofitService(Class<T> service) {
+    /**
+     * 根据传入的 Class 获取对应的 Retrofit service
+     *
+     * @param serviceClass ApiService class
+     * @param <T>          ApiService class
+     * @return ApiService
+     */
+    private <T> T getRetrofitService(Class<T> serviceClass) {
         if (mRetrofitServiceCache == null) {
             mRetrofitServiceCache = mCachefactory.build(CacheType.RETROFIT_SERVICE_CACHE);
         }
         Preconditions.checkNotNull(mRetrofitServiceCache,
                 "Cannot return null from a Cache.Factory#build(int) method");
-        T retrofitService = (T) mRetrofitServiceCache.get(service.getCanonicalName());
+        T retrofitService = (T) mRetrofitServiceCache.get(serviceClass.getCanonicalName());
         if (retrofitService == null) {
-            retrofitService = mRetrofit.get().create(service);
-            mRetrofitServiceCache.put(service.getCanonicalName(), retrofitService);
+            retrofitService = mRetrofit.get().create(serviceClass);
+            mRetrofitServiceCache.put(serviceClass.getCanonicalName(), retrofitService);
         }
         return retrofitService;
     }
@@ -114,18 +136,22 @@ public class RepositoryManager implements IRepositoryManager {
 
     /**
      * 根据传入的 Class 获取对应的 RxCache service
+     *
+     * @param cacheClass Cache class
+     * @param <T>        Cache class
+     * @return Cache
      */
     @Override
-    public synchronized <T> T obtainCacheService(Class<T> cache) {
+    public synchronized <T> T obtainCacheService(Class<T> cacheClass) {
         if (mCacheServiceCache == null) {
             mCacheServiceCache = mCachefactory.build(CacheType.CACHE_SERVICE_CACHE);
         }
         Preconditions.checkNotNull(mCacheServiceCache,
                 "Cannot return null from a Cache.Factory#build(int) method");
-        T cacheService = (T) mCacheServiceCache.get(cache.getCanonicalName());
+        T cacheService = (T) mCacheServiceCache.get(cacheClass.getCanonicalName());
         if (cacheService == null) {
-            cacheService = mRxCache.get().using(cache);
-            mCacheServiceCache.put(cache.getCanonicalName(), cacheService);
+            cacheService = mRxCache.get().using(cacheClass);
+            mCacheServiceCache.put(cacheClass.getCanonicalName(), cacheService);
         }
         return cacheService;
     }
