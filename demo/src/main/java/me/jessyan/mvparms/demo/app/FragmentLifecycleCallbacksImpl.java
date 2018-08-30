106f62513cfb3c9fe6c6f0531aa73b3aa428177d
JessYan <jessyan@foxmail.com>
2018-08-30 14:05:07 +0800

    Improve FragmentLifecycleCallbacksImpl's license

--- demo/src/main/java/me/jessyan/mvparms/demo/app/FragmentLifecycleCallbacksImpl.java
+++ demo/src/main/java/me/jessyan/mvparms/demo/app/FragmentLifecycleCallbacksImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 JessYan
+ * Copyright 2018 JessYan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
