f1071ca172079dc545e01e67d2827188def29f68
JessYan <jessyan@foxmail.com>
2018-11-21 16:34:56 +0800

    Improve GlobalConfiguration's comments

--- demo/src/main/java/me/jessyan/mvparms/demo/app/GlobalConfiguration.java
+++ demo/src/main/java/me/jessyan/mvparms/demo/app/GlobalConfiguration.java
@@ -149,20 +149,22 @@ public final class GlobalConfiguration implements ConfigModule {
 
     @Override
     public void injectAppLifecycle(Context context, List<AppLifecycles> lifecycles) {
-        // AppLifecycles 的所有方法都会在基类 Application 的对应的生命周期中被调用,所以在对应的方法中可以扩展一些自己需要的逻辑
-        // 可以根据不同的逻辑添加多个实现类
+        //AppLifecycles 中的所有方法都会在基类 Application 的对应生命周期中被调用, 所以在对应的方法中可以扩展一些自己需要的逻辑
+        //可以根据不同的逻辑添加多个实现类
         lifecycles.add(new AppLifecyclesImpl());
     }
 
     @Override
     public void injectActivityLifecycle(Context context, List<Application.ActivityLifecycleCallbacks> lifecycles) {
-        // ActivityLifecycleCallbacks 的所有方法都会在 Activity (包括三方库) 的对应的生命周期中被调用,所以在对应的方法中可以扩展一些自己需要的逻辑
-        // 可以根据不同的逻辑添加多个实现类
+        //ActivityLifecycleCallbacks 中的所有方法都会在 Activity (包括三方库) 的对应生命周期中被调用, 所以在对应的方法中可以扩展一些自己需要的逻辑
+        //可以根据不同的逻辑添加多个实现类
         lifecycles.add(new ActivityLifecycleCallbacksImpl());
     }
 
     @Override
     public void injectFragmentLifecycle(Context context, List<FragmentManager.FragmentLifecycleCallbacks> lifecycles) {
+        //FragmentLifecycleCallbacks 中的所有方法都会在 Fragment (包括三方库) 的对应生命周期中被调用, 所以在对应的方法中可以扩展一些自己需要的逻辑
+        //可以根据不同的逻辑添加多个实现类
         lifecycles.add(new FragmentLifecycleCallbacksImpl());
     }
 
