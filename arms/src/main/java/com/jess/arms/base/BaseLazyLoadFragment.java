bfc31c7352d9158ceaeb7dd2951c56f3c030a1d8
fykj_lm <1308311472@qq.com>
2019-01-07 16:01:31 +0800

    add lazyload fragment

---
+++ arms/src/main/java/com/jess/arms/base/BaseLazyLoadFragment.java
@@ -0,0 +1,73 @@
+package com.jess.arms.base;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.jess.arms.mvp.IPresenter;
+
+import java.util.List;
+
+/**
+ * 子类覆写{@link BaseLazyLoadFragment}lazyLoadData可快速实现Fragment懒加载
+ */
+public abstract class BaseLazyLoadFragment<P extends IPresenter> extends BaseFragment<P> {
+
+    private boolean isViewCreated; // 界面是否已创建完成
+    private boolean isVisibleToUser; // 是否对用户可见
+    private boolean isDataLoaded; // 数据是否已请求
+
+    /**
+     * 此处实现具体的数据请求逻辑
+     */
+    public abstract void lazyLoadData();
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        this.isVisibleToUser = isVisibleToUser;
+        tryLoadData();
+    }
+
+    /**
+     * 保证在initData后触发
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        isViewCreated = true;
+        tryLoadData();
+    }
+
+    /**
+     * ViewPager场景下，判断父fragment是否可见
+     */
+    private boolean isParentVisible() {
+        Fragment fragment = getParentFragment();
+        return fragment == null || (fragment instanceof BaseLazyLoadFragment && ((BaseLazyLoadFragment) fragment).isVisibleToUser);
+    }
+
+    /**
+     * ViewPager场景下，当前fragment可见时，如果其子fragment也可见，则让子fragment请求数据
+     */
+    private void dispatchParentVisibleState() {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        List<Fragment> fragments = fragmentManager.getFragments();
+        if (fragments.isEmpty()) {
+            return;
+        }
+        for (Fragment child : fragments) {
+            if (child instanceof BaseLazyLoadFragment && ((BaseLazyLoadFragment) child).isVisibleToUser) {
+                ((BaseLazyLoadFragment) child).tryLoadData();
+            }
+        }
+    }
+
+    public void tryLoadData() {
+        if (isViewCreated && isVisibleToUser && isParentVisible() && !isDataLoaded) {
+            lazyLoadData();
+            isDataLoaded = true;
+            //通知子Fragment请求数据
+            dispatchParentVisibleState();
+        }
+    }
+}
